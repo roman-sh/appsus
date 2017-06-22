6eba7097f836ddaca23019afcfe9f08de2a69007
klogpok <kloleg@gmail.com>
2017-06-22 11:07:45 +0300

    nav added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="app">
-        <img src="./assets/logo.png">
         <router-view></router-view>
     </div>
 </template>
@@ -8,22 +7,25 @@
 <script>
     import BookList from './components/book_store/BookList.vue';
     import Cart from './components/book_store/Cart.vue';
+    import Index from './components/Index.vue';
     export default {
         name: 'app',
         components: {
             BookList,
-            Cart
+            Cart,
+            Index
         }
     }
 </script>
 
 <style scoped>
+  html,body {
+    padding: 0;
+    margin: 0;
+  }
   #app {
       font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
       text-align: center;
       color: #2c3e50;
-      margin-top: 60px;
   }
 </style>
