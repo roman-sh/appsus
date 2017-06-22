8bcb44d06c99fff2befa39c3226429ab718e7317
Roman Shmigelsky <shmigelsky@gmail.com>
2017-06-22 14:02:51 +0300

    map half working

---
+++ src/components/maps/MainMaps.vue
@@ -0,0 +1,39 @@
+<<template>
+    <section>
+        <yandex-map :coords="[54.62896654088406, 39.731893822753904]"
+                    zoom="10"
+                    style="width: 600px; height: 600px;"
+                    lang="en-US">
+
+            <ymap-marker marker-type="placemark"
+                        :coords="[54.62896654088406, 39.731893822753904]"
+                        hint-content="Hint content 1"
+                        :balloon="{header: 'header', body: 'body', footer: 'footer'}"
+                        :icon="{color: 'green', glyph: 'cinema'}"></ymap-marker>
+
+            <ymap-marker marker-type="circle"
+                        :coords="[54.62896654088406, 39.731893822753904]"
+                        circle-radius="1600"
+                        hint-content="Hint content 1"
+                        :balloon="{header: 'header', body: 'body', footer: 'footer'}"></ymap-marker>
+        </yandex-map>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: 'main-maps',
+    // data() {
+    //   return {
+    //     activeIndex: '1',
+    //     activeIndex2: '1'
+    //   };
+    // },
+    // methods: {
+    //   handleSelect(key, keyPath) {
+    //     console.log(key, keyPath);
+    //   }
+    // }
+  }
+</script>
+
